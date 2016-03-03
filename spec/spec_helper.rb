2d43f9c4e930736d82c801bc08940cb7b47623bb
JD Guzman <jdguzman@mac.com>
2016-03-03 15:34:52 -0500

    add test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
-require 'simplecov'
-SimpleCov.start do
-  add_group 'Libraries', 'lib'
-  add_filter "/spec/"
+unless ENV['CODECLIMATE_REPO_TOKEN'].nil?
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+else
+  require 'simplecov'
+  SimpleCov.start do
+    add_group 'Libraries', 'lib'
+    add_filter "/spec/"
+  end
 end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
