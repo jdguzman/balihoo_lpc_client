726ed4ef1c2e3c58f96ea0a4e46ce9bb5d51d228
JD Guzman <jdguzman@mac.com>
2016-03-04 10:35:05 -0500

    rename var

--- lib/balihoo_lpc_client/request/authentication.rb
+++ lib/balihoo_lpc_client/request/authentication.rb
@@ -2,8 +2,8 @@ module BalihooLpcClient
   module Request
     class Authentication < Base
       def authenticate!
-        result = JSON.parse self.class.post('/genClientAPIKey')
-        auth = Response::Authentication.new(result)
+        response = JSON.parse self.class.post('/genClientAPIKey')
+        auth = Response::Authentication.new(response)
         config.client_id = auth.client_id
         config.client_api_key = auth.client_api_key
       end
