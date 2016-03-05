cedc9d74e4c7faccb0559c6bc79425416dee4206
JD Guzman <jdguzman@mac.com>
2016-03-05 10:32:22 -0500

    rename files: connection -> api

--- lib/balihoo_lpc_client.rb
+++ lib/balihoo_lpc_client.rb
@@ -3,7 +3,7 @@ require 'hashie'
 
 require 'balihoo_lpc_client/version'
 require 'balihoo_lpc_client/configuration'
-require 'balihoo_lpc_client/connection'
+require 'balihoo_lpc_client/api'
 require 'balihoo_lpc_client/request/base'
 require 'balihoo_lpc_client/request/api_base'
 require 'balihoo_lpc_client/request/authentication'
