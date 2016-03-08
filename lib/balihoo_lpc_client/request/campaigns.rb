b6f024e33777b0e776260d30b681e03dc566c479
JD Guzman <jdguzman@mac.com>
2016-03-08 16:28:39 -0500

    drying Campaigns and CampaignsWithTactics

--- lib/balihoo_lpc_client/request/campaigns.rb
+++ lib/balihoo_lpc_client/request/campaigns.rb
@@ -2,9 +2,15 @@ module BalihooLpcClient
   module Request
     class Campaigns < ApiBase
       def fetch
-        response = self.class.get('/campaigns', opts).parsed_response
+        response = get_response
         handle_errors_with(klass: ApiResponseError, response: response)
         handle_response(response: response, klass: Response::Campaign)
+      end
+
+      private
+
+      def get_response
+        self.class.get('/campaigns', opts).parsed_response
       end
     end
   end
--- lib/balihoo_lpc_client/request/campaigns_with_tactics.rb
+++ lib/balihoo_lpc_client/request/campaigns_with_tactics.rb
@@ -1,10 +1,10 @@
 module BalihooLpcClient
   module Request
-    class CampaignsWithTactics < ApiBase
-      def fetch
-        response = self.class.get('/campaignswithtactics', opts).parsed_response
-        handle_errors_with(klass: ApiResponseError, response: response)
-        handle_response(response: response, klass: Response::Campaign)
+    class CampaignsWithTactics < Campaigns
+      private
+
+      def get_response
+        self.class.get('/campaignswithtactics', opts).parsed_response
       end
     end
   end
